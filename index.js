14957fcf77b2e37b54c33c184ff2cee832d14212
islamulhaque <islamulbd24@gmail.com>
2025-07-24 15:39:11 +0600

    added otp page

--- index.js
+++ index.js
@@ -32,3 +32,18 @@
         menuOpen = !menuOpen;
       });
     
+
+        // Simple auto-focus switching between OTP inputs
+        document.querySelectorAll('input[type="text"]').forEach((input, index, inputs) => {
+            input.addEventListener('input', (e) => {
+                if (e.target.value.length === 1 && index < inputs.length - 1) {
+                    inputs[index + 1].focus();
+                }
+            });
+            
+            input.addEventListener('keydown', (e) => {
+                if (e.key === 'Backspace' && e.target.value.length === 0 && index > 0) {
+                    inputs[index - 1].focus();
+                }
+            });
+        });
